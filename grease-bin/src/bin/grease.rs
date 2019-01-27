e5502bd84d31f5b2bfad03c9e92e260f49d10579
Kent Fredric <kentnl@gentoo.org>
2019-01-27 14:39:02 +1300

    Factor logic into grease::repository::Repository

--- grease-bin/src/bin/grease.rs
+++ grease-bin/src/bin/grease.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 
 fn main() {
     let p = Path::new("/usr/local/gentoo");
-    let iter = grease::Repository::new(p).categories();
+    let iter = grease::repository::Repository::new(p).categories();
     for ent in iter.unwrap().into_iter() {
         println!("> {}", ent);
     }
