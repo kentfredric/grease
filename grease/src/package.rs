4a510eee17faad12167b2a1a20277deed2123e3d
Kent Fredric <kentnl@gentoo.org>
2019-02-09 23:10:57 +1300

    Iterative implementation of ebuild_iterator to get essential logic right

--- grease/src/package.rs
+++ grease/src/package.rs
@@ -17,11 +17,30 @@ fn in_category_dir(category_root: &path::Path) -> PackageIterResult {
                 // readdir entry failures passthrough
                 true
             })
+
             // Munge Ok(), passthru Err()
-            .map(|e| e.map( |ent| ent.file_name() ) )
+            .map(|e| e.map( |ent| ent.file_name() ) ),
     ))
 }
 
 pub fn iterator(root: &'static path::Path, category: &ffi::OsStr) -> PackageIterResult {
     in_category_dir(&root.join(category))
+}
+
+pub fn ebuild_iterator(root: &'static path::Path, category: &ffi::OsStr) -> PackageIterResult {
+    let packages = iterator(&root, &category)?;
+    let mut out = Vec::new();
+    for packageResult in packages {
+        let package = packageResult?;
+        let ebuilds = super::ebuild::iterator(&root, &category, &package)?;
+        for ebuildResult in ebuilds {
+            let ebuild = ebuildResult?;
+            out.push(Ok(ffi::OsString::from(format!(
+                "{}/{}",
+                package.clone().into_string().unwrap(),
+                ebuild.into_string().unwrap()
+            ))));
+        }
+    }
+    Ok(Box::new(out.into_iter()))
 }
