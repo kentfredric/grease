7d7128290a434a2cb960bd4659d414cc46dab276
Kent Fredric <kentnl@gentoo.org>
2019-02-13 18:04:14 +1300

    use object-trait iterator

--- grease-bin/src/bin/grease.rs
+++ grease-bin/src/bin/grease.rs
@@ -10,7 +10,8 @@ fn main() -> std::result::Result<(), std::io::Error> {
     let p = Path::new("/usr/local/gentoo");
     for ent in grease::category::iterator(p)? {
         let ent_u = ent?;
-        for pkg in grease::package::iterator(p, &ent_u)? {
+        println!("{:?}", ent_u);
+        for pkg in ent_u.packages()? {
             let pkg_u = pkg?;
             println!("{:?}", pkg_u);
             for ebuild in pkg_u.ebuilds()? {
