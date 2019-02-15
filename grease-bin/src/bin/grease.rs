4944b4ed65825a203782c395fbf7b9213a1ca8f5
Kent Fredric <kentnl@gentoo.org>
2019-02-15 20:30:55 +1300

    Use new APIs in grease

--- grease-bin/src/bin/grease.rs
+++ grease-bin/src/bin/grease.rs
@@ -1,9 +1,9 @@
 extern crate grease;
 
 use grease::repository::Repository;
+use std::io::Error;
 use std::path::Path;
 use std::result::Result;
-use std::io::Error;
 
 #[global_allocator]
 static GLOBAL: std::alloc::System = std::alloc::System;
@@ -11,18 +11,8 @@ static GLOBAL: std::alloc::System = std::alloc::System;
 fn main() -> Result<(), Error> {
     let p = Path::new("/usr/local/gentoo");
     let r = Repository::new(p);
-    for cat in r.categories()? {
-        let cat_u = cat?;
-        println!("{:?}", cat_u);
-        for pkg in cat_u.packages()? {
-            let pkg_u = pkg?;
-            println!("{:?}", pkg_u);
-            for ebuild in pkg_u.ebuilds()? {
-                if let Ok(e) = ebuild {
-                    println!("{:?}", e);
-                }
-            }
-        }
+    for ebuild in r.ebuilds()? {
+        println!("{:?}", ebuild?);
     }
     Ok(())
 }
