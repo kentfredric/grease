1e9aa6196c1be803ace970592b22544f6077d7ad
Kent Fredric <kentnl@gentoo.org>
2019-02-17 02:11:55 +1300

    Simplify Versions

--- src/version.rs
+++ src/version.rs
@@ -1,36 +1,34 @@
-use std::ffi::OsString;
-
+#[derive(Clone)]
 pub struct Version {
-    version: OsString,
     pv: String,
     pr: String,
 }
 
 impl Version {
-    pub fn new(version: OsString) -> Version {
-        let v_string = version.to_str().map(String::from).expect(
-            "Can't decode OsString \
-             to UTF8",
-        );
-        let mut v_chunks: Vec<_> = v_string.split_terminator("-r").collect();
-        let tail = v_chunks.pop();
-
-        if v_chunks.len() > 1 && tail.is_some() &&
-            tail.expect("no value returned from string iterator")
-                .parse::<u32>()
-                .is_ok()
-        {
-            Version {
-                version,
-                pv: v_string,
-                pr: String::from("r0"),
-            }
-        } else {
-            Version {
-                version,
-                pv: v_chunks.join("-"),
-                pr: String::from("r".to_owned() + tail.unwrap()),
-            }
-        }
+    pub fn new(pv: String, pr: String) -> Version { Version { pv, pr } }
+    pub fn pv(&self) -> &str { &self.pv }
+    pub fn pr(&self) -> &str { &self.pr }
+    pub fn pvr(&self) -> String {
+       self.pv().to_owned() +  "-"  + self.pr()
     }
+}
+
+pub fn parse(version: &str) -> Version {
+    let vx = version.to_string();
+    let mut v_chunks: Vec<_> = vx.split_terminator("-r").collect();
+    let tail = v_chunks.pop();
+    if v_chunks.len() > 0 && tail.is_some() &&
+        tail.expect("no value returned from string iterator")
+            .parse::<u32>()
+            .is_ok()
+    {
+        Version::new(
+            v_chunks.join("-"),
+            String::from("r".to_owned() + tail.unwrap()),
+        )
+    } else {
+        Version::new(vx.to_owned(), String::from("r0"))
+    }
+
+
 }
