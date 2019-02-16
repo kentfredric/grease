355a08cea5e15758a4bdab29de6eae0b6f8f26ba
Kent Fredric <kentnl@gentoo.org>
2019-02-16 15:26:13 +1300

    Massively simplify version parsing logic

--- src/version.rs
+++ src/version.rs
@@ -1,59 +1,36 @@
-use std::cell::RefCell;
 use std::ffi::OsString;
-use std::option::Option;
 
 pub struct Version {
     version: OsString,
-    pv: RefCell<Option<Option<String>>>,
-    pr: RefCell<Option<Option<String>>>,
+    pv: String,
+    pr: String,
 }
 
 impl Version {
-    pub fn new(version: OsString) -> Version { Version { version , pv: RefCell::new(None), pr: RefCell::new(None)} }
-    pub fn pv(&self) -> Option<String> {
-        if self.pv.borrow().is_none() {
-            self.pv.replace(Some(self.pvr().map(|pvr| {
-                let mut chunks: Vec<_> = pvr.split_terminator("-r").collect();
-                if chunks.len() < 2 {
-                    pvr
-                } else {
-                    match chunks.pop() {
-                        None => pvr,
-                        Some(rversion) => {
-                            match rversion.parse::<u32>() {
-                                Ok(_) => chunks.join("-"),
-                                Err(_) => pvr,
-                            }
-                        },
-                    }
-                }
-            })));
+    pub fn new(version: OsString) -> Version {
+        let v_string = version.to_str().map(String::from).expect(
+            "Can't decode OsString \
+             to UTF8",
+        );
+        let mut v_chunks: Vec<_> = v_string.split_terminator("-r").collect();
+        let tail = v_chunks.pop();
+
+        if v_chunks.len() > 1 && tail.is_some() &&
+            tail.expect("no value returned from string iterator")
+                .parse::<u32>()
+                .is_ok()
+        {
+            Version {
+                version,
+                pv: v_string,
+                pr: String::from("r0"),
+            }
+        } else {
+            Version {
+                version,
+                pv: v_chunks.join("-"),
+                pr: String::from("r".to_owned() + tail.unwrap()),
+            }
         }
-        self.pv.borrow().to_owned().unwrap_or(None)
     }
-    pub fn pr(&self) -> Option<String> {
-        if self.pr.borrow().is_none() {
-            self.pr.replace(Some(self.pvr().map(|pvr| {
-                let mut chunks: Vec<_> = pvr.split_terminator("-r").collect();
-                // This finds the trailing block of PVR which *might* be "-r" + a series of
-                // digits if such a block exists, return the r-suffix, otherwise, perform no
-                // changes and return PVR as PV
-                if chunks.len() < 2 {
-                    String::from("r0")
-                } else {
-                    match chunks.pop() {
-                        None => String::from("r0"),
-                        Some(rversion) => {
-                            match rversion.parse::<u32>() {
-                                Ok(_) => String::from("r".to_owned() + rversion),
-                                Err(_) => String::from("r0"),
-                            }
-                        },
-                    }
-                }
-            })));
-        }
-        self.pr.borrow().to_owned().unwrap_or(None)
-    }
-    pub fn pvr(&self) -> Option<String> { self.version.to_str().map(String::from) }
 }
