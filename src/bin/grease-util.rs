8861411850ab5ee724b546eb446245650befa798
Kent Fredric <kentnl@gentoo.org>
2019-03-01 10:28:11 +1300

    Allow packages and ebuild iterators to filter by category

--- src/bin/grease-util.rs
+++ src/bin/grease-util.rs
@@ -27,21 +27,51 @@ fn main() {
         (@setting SubcommandRequired)
         (@subcommand iterate =>
             (about: "Traverse all kinds of a thing in a given repository")
-            (@arg REPOSITORY: -r --repository +required +takes_value visible_alias[repo]
+            (@arg REPOSITORY: -r --repository
+                +required +takes_value
+                visible_alias[repo]
                 "Specifies the repository to iterate"
             )
             (@setting SubcommandRequired)
             (@subcommand categories =>
                 (about: "Iterate all categories in a repository")
-                (@arg FORMATTER: -f --formatter +takes_value possible_value[path ident components name] default_value[ident])
+                (@arg FORMATTER: -f --formatter
+                    +takes_value
+                    possible_value[path ident components name]
+                    default_value[ident]
+                    visible_alias[format]
+                    "Controls the presentation of output"
+                )
             )
             (@subcommand packages =>
                 (about: "Iterate all packages in a repository")
-                (@arg FORMATTER: -f --formatter +takes_value possible_value[path ident components name] default_value[ident])
+                (@arg FORMATTER: -f --formatter
+                    +takes_value
+                    possible_value[path ident components name]
+                    default_value[ident]
+                    visible_alias[format]
+                    "Controls the presentation of output"
+                )
+                (@arg CATEGORY: -c --category [CATEGORY_NAME] ...
+                    +takes_value #{1,100} +require_delimiter
+                    visible_alias[cat]
+                    "Restrict categories to a list provided"
+                )
             )
             (@subcommand ebuilds =>
                 (about: "Iterate all ebuilds in a repository")
-                (@arg FORMATTER: -f --formatter +takes_value possible_value[path ident components name] default_value[ident])
+                (@arg FORMATTER: -f --formatter
+                    +takes_value
+                    possible_value[path ident components name]
+                    default_value[ident]
+                    visible_alias[format]
+                    "Controls the presentation of output"
+                )
+                (@arg CATEGORY: -c --category [CATEGORY_NAME] ...
+                    +takes_value #{1,100} +require_delimiter
+                    visible_alias[cat]
+                    "Restrict categories to a list provided"
+                )
             )
         )
     )
@@ -67,17 +97,47 @@ fn iter_repo_categories(repo: &str, opts: &ArgMatches) {
 }
 fn iter_repo_packages(repo: &str, opts: &ArgMatches) {
     let r = Repository::new(Path::new(repo));
-    let citer = r.packages().expect("Error reading ebuilds from repo");
     let formatter = repoobject::parse_formatter(opts.value_of("FORMATTER").unwrap()).unwrap();
-    for it in citer.filter_oks(Package::is_legal).extract_errs(|e| panic!(e)) {
-        println!("{}", it.render(&formatter));
+    if opts.is_present("CATEGORY") {
+        let values_iter = opts.values_of("CATEGORY").unwrap();
+        for cat in values_iter {
+            let c = r.get_category(cat);
+            if !c.is_legal() {
+                panic!("{:?} is not legal in the given repository", c);
+            }
+            let citer = c.packages().expect("Error reading packages from repo");
+            for it in citer.filter_oks(Package::is_legal).extract_errs(|e| panic!(e)) {
+                println!("{}", it.render(&formatter));
+            }
+        }
+    } else {
+        let citer = r.packages().expect("Error reading ebuilds from repo");
+        for it in citer.filter_oks(Package::is_legal).extract_errs(|e| panic!(e)) {
+            println!("{}", it.render(&formatter));
+        }
     }
 }
 fn iter_repo_ebuilds(repo: &str, opts: &ArgMatches) {
     let r = Repository::new(Path::new(repo));
-    let citer = r.ebuilds().expect("Error reading ebuilds from repo");
     let formatter = repoobject::parse_formatter(opts.value_of("FORMATTER").unwrap()).unwrap();
-    for it in citer.filter_oks(Ebuild::is_legal).extract_errs(|e| panic!(e)) {
-        println!("{}", it.render(&formatter));
+
+    let citer = r.ebuilds().expect("Error reading ebuilds from repo");
+
+    if opts.is_present("CATEGORY") {
+        let values_iter = opts.values_of("CATEGORY").unwrap();
+        for cat in values_iter {
+            let c = r.get_category(cat);
+            if !c.is_legal() {
+                panic!("{:?} is not legal in the given repository", c);
+            }
+            let citer = c.ebuilds().expect("Error reading ebuilds from repo");
+            for it in citer.filter_oks(Ebuild::is_legal).extract_errs(|e| panic!(e)) {
+                println!("{}", it.render(&formatter));
+            }
+        }
+    } else {
+        for it in citer.filter_oks(Ebuild::is_legal).extract_errs(|e| panic!(e)) {
+            println!("{}", it.render(&formatter));
+        }
     }
 }
