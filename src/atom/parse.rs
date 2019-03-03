ee053feac8fa504fdad9f87c66b5d3ec107cfad6
Kent Fredric <kentnl@gentoo.org>
2019-03-03 17:55:00 +1300

    Add return error handling with inline test

--- src/atom/parse.rs
+++ src/atom/parse.rs
@@ -6,6 +6,25 @@ use crate::{
     err::AtomParseError,
 };
 
+#[test]
+fn parse_category() {
+    match category("dev-perl") {
+        Ok(c) => assert_eq!(c.category, "dev-perl"),
+        Err(_) => assert!(false),
+    }
+    match category("virtual") {
+        Ok(c) => assert_eq!(c.category, "virtual"),
+        Err(_) => assert!(false),
+    }
+    match category("-invalid") {
+        Ok(c) => assert_ne!(c.category, "-invalid"),
+        Err(e) => match e {
+            AtomParseError::BadCategory(c) => assert_eq!(c, "-invalid"),
+            AtomParseError::BadPackage(_) => assert!(false),
+        },
+    }
+}
+
 /** Decode a string into a [`Category`]
 
 **/
@@ -13,6 +32,6 @@ pub fn category(c: &str) -> Result<Category, AtomParseError> {
     if validate::category_name(c) {
         Ok(Category { category: c.to_owned() })
     } else {
-        unimplemented!()
+        Err(AtomParseError::BadCategory(c.to_owned()))
     }
 }
